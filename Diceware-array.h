413b005f6445df8511d9ddc60234778153e63462
Doug P <dougtko@gmail.com>
2011-04-15 01:51:01 -0400

    comparing lib size when using an array vs map for wordlist

---
+++ Diceware-array.h
@@ -0,0 +1,26 @@
+/*
+ * Diceware library for eventual use 
+ * on an Arduino.
+ *
+ * Doug Prostko
+ *
+ */
+#ifndef Diceware_h
+#define Diceware_h
+
+// #include "WProgram.h"
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+class Diceware
+{
+    public:
+        Diceware();
+        string getWord(int roll);
+    private:
+        int _roll;
+};
+
+#endif
